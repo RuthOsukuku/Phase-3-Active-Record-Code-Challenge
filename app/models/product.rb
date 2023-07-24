baa99a5eabb265df5962f7e16dad2a97593d7fb1
Ruth <osukukuruth@gmail.com>
2023-07-24 12:09:38 -0400

    Done migration

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,27 @@
-
 class Product < ActiveRecord::Base
-    has_many :reviews
-    has_many :users, through: :reviews
+  # ...
+
+  def leave_review(user, star_rating, comment)
+    reviews.create(user: user, star_rating: star_rating, comment: comment)
   end
+
+  def print_all_reviews
+    reviews.each(&:print_review)
+  end
+
+  def average_rating
+    reviews.average(:star_rating).to_f
+  end
+end
+
+class User < ActiveRecord::Base
+  # ...
+
+  def favorite_product
+    reviews.order(star_rating: :desc).first&.product
+  end
+
+  def remove_reviews(product)
+    reviews.where(product: product).destroy_all
+  end
+end
--- db/migrate/20200110172038_create_users.rb
+++ db/migrate/20200110172038_create_users.rb
@@ -1,7 +1,18 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :users do |t|
-      t.string :name
-    end
+class User < ActiveRecord::Base
+  has_many :reviews
+  has_many :products, through: :reviews
+end
+
+class Product < ActiveRecord::Base
+  has_many :reviews
+  has_many :users, through: :reviews
+end
+
+class Review < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :product
+
+  def print_review
+    puts "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
   end
 end
